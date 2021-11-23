da7f5891161618ccddf0ebd4eec10e02c24ec934
Ansh16012000 <79369429+Ansh16012000@users.noreply.github.com>
2021-11-23 15:50:13 +0530

    footer creation

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import "./Footer.css";
 
 export const Footer = () => {
     return (
         <div>
-            <h1>footer</h1>
+            <footer>
+                <div className="container">
+                    <div className="row">
+                        <h4>Join our exclusive membership to receive the latest news and trends</h4>
+                    </div>
+                </div>
+            </footer>
         </div>
     )
 }
