f74c80721be3af4104abd2bd7ecb172d0bd467c3
Ansh16012000 <79369429+Ansh16012000@users.noreply.github.com>
2021-11-23 18:39:02 +0530

    footer responsive

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React from "react";
 import "./Footer.css";
 
 export const Footer = () => {
-    return (
-        <div>
-            <footer>
-                <div className="container">
-                    <div className="row">
-                        <h4>Join our exclusive membership to receive the latest news and trends</h4>
-                    </div>
-                </div>
-            </footer>
+  return (
+    <div>
+      <footer>
+        <div className="container">
+          <div className="row textrow">
+            <h4>
+              Join our exclusive membership to receive the latest news and
+              trends
+            </h4>
+            <br />
+            <h5>You can unsubscribe at any time.</h5>
+          </div>
+            <div className="row searchboxrow">
+            <div className="col-8 searchbox">
+              <div class="input-group">
+                <input
+                  type="search"
+                  class="form-control rounded "
+                  placeholder="Your Email"
+                  aria-label="Search"
+                  aria-describedby="search-addon"
+                />
+              </div>
+            </div>
+            <div className="col-4">
+              <button type="button" class="btn bg-transparent subscribebtn ">
+                Subscribe
+              </button>
+            </div>
+            </div>
         </div>
-    )
-}
+      </footer>
+    </div>
+  );
+};
--- src/Components/Header/NavBar.js
+++ src/Components/Header/NavBar.js
@@ -7,28 +7,27 @@ export const NavBar = () => {
     <nav className="navbar">
       <div className="container">
         <div className="row">
-          <div className="col-xl-6">
+          <div className="col-md-6 col-xs-6">
             <div className="row one">
               <ul className="navbar-links ">
 
-              <div className="col-xl-6">
+              <div className="col-md-4 col-xs-4">
               <li className="nav-item">
                   
                 <FaFingerprint className="finger" />
                 </li>
               </div>
-              <div className="col-xl-6">
+              <div className="col-md-8 col-xs-8">
               <li className="nav-item">
                 <h4 className="name">Ansh</h4>
                 </li>
-                
               </div>
               </ul>
             </div>
             </div>
-          <div className="col-xl-6 ">
+          <div className="col-md-6 col-xs-6 ">
             <div className="row">
-              <div className="col-xl-10">
+              <div className="col-md-10 col-xs-10">
               <ul  className="links navbar-links ">
               <li className="nav-item liitem">
                 <a className="item navhome">Home</a>
@@ -41,7 +40,7 @@ export const NavBar = () => {
               </li>
             </ul>
               </div>
-              <div className="col-xl-2">
+              <div className="col-xl-2 col-xs-2">
                 <ul className="navbar-links ">
               <li className="navsignup"><button className="btn bg-transparent signup">SIGNUP</button></li>
                 </ul>
